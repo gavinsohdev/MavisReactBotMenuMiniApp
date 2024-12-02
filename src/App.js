7d5640eed41c43129ce22560311b88451b0ababc
vandergav <gavinsohdev@gmail.com>
2024-12-02 19:40:29 +0800

    Firebase 24

--- src/App.js
+++ src/App.js
@@ -11,29 +11,94 @@ import axios from "axios";
 const App = () => {
   const showPopup = useShowPopup();
   const [userData, setUserData] = useState({});
-  const [userFirebaseData, setUserFirebaseData] = useState([]);
-  const [page, setPage] = useState(1); // Tracks the current page for navigation
-  const [loading, setLoading] = useState(true); // Tracks loading state
+  const [userFirebaseData, setUserFirebaseData] = useState("");
+  const [page, setPage] = useState(1);
+  const [coin, setCoin] = useState(0);
+  const [isRegistered, setIsRegistered] = useState(false);
 
   const [impactOccurred, notificationOccurred, selectionChanged] =
     useHapticFeedback();
   const [InitDataUnsafe, InitData] = useInitData();
 
   useEffect(() => {
-    setTimeout(() => {
-      const tg = window.Telegram.WebApp;
-      tg.ready();
-      setLoading(false);
-      const { photo_url, first_name, last_name } = InitDataUnsafe?.user || {};
-      setUserData({ photo_url, first_name, last_name });
+    const tg = window.Telegram.WebApp;
+    tg.ready();
+    const { photo_url, first_name, last_name } = InitDataUnsafe?.user || {};
+    setUserData({ photo_url, first_name, last_name });
+    fetchAndUpdateUserData();
 
-      tg.onEvent("mainButtonClicked", function () {
-        const dataToSend = { status: "clicked", timestamp: Date.now() };
-        tg.sendData(JSON.stringify(dataToSend));
-        tg.close();
-      });
-    }, 500);
+    tg.onEvent("mainButtonClicked", function () {
+      const dataToSend = { status: "clicked", timestamp: Date.now() };
+      tg.sendData(JSON.stringify(dataToSend));
+      tg.close();
+    });
   }, [InitDataUnsafe]);
+
+  const handleGetUserCoins = async () => {
+    impactOccurred("light");
+    const payload = {
+      id: String(InitDataUnsafe?.user?.id),
+    };
+    if (payload.id) {
+      try {
+        const response = await axios.post("/api/get-coins", payload, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        setCoin(response?.data?.coin);
+      } catch (error) {
+        console.error("Error submitting data:", error);
+      }
+    } else {
+      console.error("ID is invalid");
+    }
+  };
+
+  async function fetchAndUpdateUserData() {
+    const { status = false, dataArr = [] } = await handleGetUser();
+    if (status) {
+      handleGetUserCoins();
+      setIsRegistered(status);
+      const dataChanged = hasDataChanged(InitDataUnsafe.user, dataArr[0]);
+      console.log("Old: " + JSON.stringify(InitDataUnsafe.user));
+      console.log("New: " + JSON.stringify(dataArr[0]));
+      console.log("dataChanged: " + dataChanged);
+      dataChanged && handleUpload();
+    } else {
+      setIsRegistered(status);
+    }
+  }
+
+  function hasDataChanged(newData, oldData) {
+    // Clone the objects to avoid modifying the original data
+    const newClone = { ...newData };
+    const oldClone = { ...oldData };
+
+    // Remove the keys to ignore
+    delete newClone.allows_write_to_pm;
+
+    console.log("newClone: " + JSON.stringify(newClone));
+    console.log("oldClone: " + JSON.stringify(oldClone));
+
+    // Deep comparison of objects
+    const areObjectsEqual = (obj1, obj2) => {
+      const keys1 = Object.keys(obj1);
+      const keys2 = Object.keys(obj2);
+
+      // Check if both objects have the same keys
+      if (keys1.length !== keys2.length) return false;
+
+      // Check each key-value pair
+      for (const key of keys1) {
+        if (obj1[key] !== obj2[key]) return false;
+      }
+
+      return true;
+    };
+
+    return !areObjectsEqual(newClone, oldClone);
+  }
 
   // Handle the back button click
   const handleBackButtonClick = () => {
@@ -61,7 +126,7 @@ const App = () => {
 
   const handleSubmit = async () => {
     const payload = {
-      chatId: InitDataUnsafe?.user?.id || "defaultChatId",
+      chatId: InitDataUnsafe?.user?.id,
       message: "hi",
     };
     try {
@@ -88,25 +153,32 @@ const App = () => {
     });
   };
 
+  function getPayload(InitDataUnsafe) {
+    return {
+      id: String(InitDataUnsafe?.user?.id),
+      first_name: InitDataUnsafe?.user?.first_name,
+      last_name: InitDataUnsafe?.user?.last_name,
+      username: InitDataUnsafe?.user?.username,
+      language_code: InitDataUnsafe?.user?.language_code,
+      photo_url: InitDataUnsafe?.user?.photo_url,
+    };
+  }
+
   const handleUpload = async () => {
     impactOccurred("light");
-    const payload = {
-      id: InitDataUnsafe?.user?.id || "defaultChatId",
-      firstName: InitDataUnsafe?.user?.first_name || "defaultFirstName",
-      lastName: InitDataUnsafe?.user?.last_name || "defaultLastName",
-      username: InitDataUnsafe?.user?.username || "defaultUsername",
-      language: InitDataUnsafe?.user?.language_code || "defaultLanguageCode",
-      photoUrl: InitDataUnsafe?.user?.photo_url || "defaultPhotoUrl",
-    };
+    const payload = getPayload(InitDataUnsafe);
     try {
       const response = await axios.post("/api/test-upload", payload, {
         headers: {
           "Content-Type": "application/json",
         },
       });
-      response?.status
-        ? handlePopup({ title: "Info", message: "Success" })
-        : handlePopup({ title: "Info", message: "Failure" });
+      if (response?.status) {
+        setUserFirebaseData(getPayload(InitDataUnsafe));
+        setIsRegistered(true);
+      } else {
+        handlePopup({ title: "Info", message: "Failure" });
+      }
     } catch (error) {
       console.error("Error submitting data:", error);
     }
@@ -115,7 +187,7 @@ const App = () => {
   const handleGetUser = async () => {
     impactOccurred("light");
     const payload = {
-      id: InitDataUnsafe?.user?.id || null,
+      id: String(InitDataUnsafe?.user?.id),
     };
     if (payload.id) {
       try {
@@ -124,7 +196,12 @@ const App = () => {
             "Content-Type": "application/json",
           },
         });
-        response?.status && setUserFirebaseData(response.dataArr)
+        if (response?.data?.status) {
+          setUserFirebaseData(response.data.dataArr[0]);
+          return response.data;
+        } else {
+          return { status: false };
+        }
       } catch (error) {
         console.error("Error submitting data:", error);
       }
@@ -166,19 +243,62 @@ const App = () => {
     }
   };
 
-  // If the app is loading, show the loading screen
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-200">
-        <div className="text-center">
-          <div className="loader rounded-full border-t-4 border-blue-500 w-16 h-16 animate-spin"></div>
-          <p className="mt-4 text-lg text-gray-700">Loading...</p>
+  return isRegistered ? (
+    <div className="min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 flex flex-col items-center justify-center p-6">
+      <div className="text-center mb-8">
+        <h1 className="text-3xl font-bold text-gray-800">
+          Welcome back, {userData?.first_name}
+        </h1>
+        <p className="text-lg text-gray-600 mt-2">
+          We're glad to see you again!
+        </p>
+      </div>
+      <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-2xl">
+        <div className="space-y-8">
+          {/* User Data List */}
+          <ul className="space-y-4">
+            {[
+              "photo_url",
+              "first_name",
+              "last_name",
+              "username",
+              "language_code",
+              "id",
+            ].map((key) => (
+              <li key={key} className="flex items-center space-x-4">
+                <span className="font-medium text-gray-600 capitalize w-32">
+                  {key.replace(/_/g, " ")}:
+                </span>
+                {key === "photo_url" ? (
+                  <img
+                    src={userFirebaseData[key]}
+                    alt={`${userFirebaseData.first_name}'s avatar`}
+                    className="w-16 h-16 rounded-full border border-gray-300"
+                  />
+                ) : (
+                  <span className="text-gray-800">{userFirebaseData[key]}</span>
+                )}
+              </li>
+            ))}
+          </ul>
+
+          {/* Coin Display */}
+          <div className="flex items-center justify-center space-x-4 bg-gradient-to-r from-yellow-100 to-yellow-200 p-4 rounded-lg shadow-lg">
+            <div className="relative">
+              {/* Animated Coin */}
+              <div className="w-10 h-10 rounded-full bg-yellow-500 border-2 border-yellow-700 flex items-center justify-center text-yellow-100 font-bold animate-spin-slow">
+                💰
+              </div>
+            </div>
+            {/* Coin Count */}
+            <div className="text-gray-800 text-lg font-semibold">
+              Coins: <span className="text-yellow-600 font-bold">{coin}</span>
+            </div>
+          </div>
         </div>
       </div>
-    );
-  }
-
-  return (
+    </div>
+  ) : (
     <div className="min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 flex flex-col items-center justify-center p-6">
       <BackButton onClick={handleBackButtonClick} />
       <MainButton onClick={handleMainButtonClick} text="Main Button" />
@@ -236,7 +356,7 @@ const App = () => {
             onClick={handleUpload}
             className="w-full bg-pink-500 text-white py-3 px-4 rounded-lg text-lg font-medium shadow-md hover:bg-pink-600 transition duration-200"
           >
-            Test Upload
+            Register
           </button>
           <button
             onClick={handleGetUser}
@@ -244,19 +364,10 @@ const App = () => {
           >
             Test Get User
           </button>
-          <div>
-            <p className="w-4/5 mx-auto text-center text-gray-700 text-lg">
-              {JSON.stringify(InitDataUnsafe)}
-            </p>
-          </div>
         </div>
         <div>
           <h2>Firebase Data: </h2>
-          <ul>
-            {userFirebaseData.map((x) => (
-              <li key={x.id}>{x}</li>
-            ))}
-          </ul>
+          <ul>{JSON.stringify(userFirebaseData)}</ul>
         </div>
       </div>
     </div>
